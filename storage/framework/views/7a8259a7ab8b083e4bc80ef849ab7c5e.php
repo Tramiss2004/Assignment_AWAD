c171e1f4c71cd55cc425507dd899c92f2a5fd82d
Tramiss2004 <138105907+Tramiss2004@users.noreply.github.com>
2025-02-19 11:59:36 +0800

    setup the laravel (except one of the folder)

---
+++ storage/framework/views/7a8259a7ab8b083e4bc80ef849ab7c5e.php
@@ -0,0 +1,9 @@
+<DOCTYPE! html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <h1>Main Page</h1>
+    </body>
+</html><?php /**PATH C:\Degree course\Year 3 Sem 1\Advanced Web Application Development\Assignment\Assignment_AWAD\resources\views/MainPage.blade.php ENDPATH**/ ?>
